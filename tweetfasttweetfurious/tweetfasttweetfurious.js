0e0513144735cc2620cfec0b98f5b34764260a6a
John Feminella <johnf.services.github@accounts.distilledb.com>
2009-06-20 12:59:14 -0400

    Adds initial headers.

---
+++ tweetfasttweetfurious/tweetfasttweetfurious.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name           TweetFastTweetFurious
+// @namespace      http://distilledb.com
+// @description    Has your honor been scandalously impugned by an insolent Twitter-whelp? Show that cad how you feel by challenging them to a TweetsOfFury battle, post-haste!
+// @include        http://twitter.com/*
+// @include        http://tweetsoffury.com/*
+// @require        http://jqueryjs.googlecode.com/files/jquery-1.3.2.min.js  
+// ==/UserScript==
